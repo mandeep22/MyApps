d511c1381d63958e37d27ff6a8ef80b2b10c5fbd
mandeep22 <mandeep.jammu@gmail.com>
2016-11-24 22:29:15 -0600

    App completed.

--- app/src/main/java/com/example/android/myapps/MainActivity.java
+++ app/src/main/java/com/example/android/myapps/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.myapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This button will launch my popular movies project", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This button will launch my stock hawk project", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This button will launch my build it bigger project", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This button will launch my make your own app material project", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This button will launch my go ubiquitous project", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This button will launch my capstone project", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
